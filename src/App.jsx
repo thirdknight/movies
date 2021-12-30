642b9afc6349c76f3c3f50a07877efc8bcf60fd6
Edinson <edinson.ningles@gmail.com>
2021-12-31 00:43:46 +0100

    Add Landing Page and movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { MoviesGrid } from "./components/MoviesGrid";
-import styles from './App.module.css';
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MoviesDetails } from "./pages/MoviesDetails";
+import { LandingPage } from "./pages/LandingPage";
 
-export function App () {
+export function App() {
   return (
-    <div>
-     <header>
-       <h1 className={styles.title}>Movies</h1>
-     </header>
-     <main>
-      <MoviesGrid />
-     </main>
-    </div>
-  )
+    <Router>
+      <header>
+        <Link>
+          <h1 className={styles.title}>Movies</h1>
+        </Link>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/movies/:movieId"><MoviesDetails /></Route>
+          <Route path="/"><LandingPage /></Route>
+        </Switch>
+      </main>
+    </Router>
+  );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.css";
 
 export function MovieCard({ movie }) {
@@ -5,13 +6,16 @@ export function MovieCard({ movie }) {
   // console.log(styles);
   return (
     <li className={styles.movieCard}>
-      <img 
-        width={230} 
-        height={345}
-        className={styles.movieImage} 
-        src={imageUrl} 
-        alt={movie.title} />
+      <Link to={"/movies/" + movie.id }>
+        <img 
+          width={230} 
+          height={345}
+          className={styles.movieImage} 
+          src={imageUrl} 
+          alt={movie.title}
+        />
       <div>{movie.title}</div>
+      </Link>
     </li>
   );
 }
