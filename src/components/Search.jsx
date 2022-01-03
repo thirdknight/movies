ab486e1e6f5318ed536396b6f37cd1e51a7ab619
Edinson <edinson.ningles@gmail.com>
2022-01-03 16:08:02 +0100

    add search styles search button

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import styles from './Search.module.css';
+import { FaSearch } from 'react-icons/fa';
+
+export function Search() {
+  return(
+      <form className={styles.searchContainer}>
+        <div className={styles.searchBox}>
+          <input className={styles.searchInput} type="text" />
+          <button className={styles.searchButton} type="submit">
+            <FaSearch size={20} />
+          </button>
+        </div>
+      </form>
+  )
+}
